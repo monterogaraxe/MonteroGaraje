7ce0763df95f75f2fea1f6b0b6071203a3442af9
monterogaraxe <infomonterogaraxe@gmail.com>
2026-07-18 00:31:02 +0200

    Add test database of car objects
    Added a test database of car objects with details.

--- app.js
+++ app.js
@@ -15,3 +15,26 @@ const estado = {
 
 console.log("Versión:", version);
 console.log("Estado:", estado);
+const coches = [
+  {
+    marca: "Volkswagen",
+    modelo: "Golf GTI MK2",
+    pais: "España",
+    precio: 18000
+  },
+  {
+    marca: "Audi",
+    modelo: "RS3",
+    pais: "Alemania",
+    precio: 52000
+  },
+  {
+    marca: "Cupra",
+    modelo: "Formentor VZ5",
+    pais: "Países Bajos",
+    precio: 47000
+  }
+];
+
+console.log("Base de datos de prueba:");
+console.log(coches);
