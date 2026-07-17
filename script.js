22e742575f8c334b7c3dc7230066a64806f23484
monterogaraxe <infomonterogaraxe@gmail.com>
2026-07-18 00:07:54 +0200

    Add array of car objects and log to console

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 script.js
+const coches = [
+  {
+    marca: "Volkswagen",
+    modelo: "Golf GTI MK2",
+    pais: "España",
+    precio: 18000
+  },
+  {
+    marca: "Audi",
+    modelo: "RS3",
+    pais: "Alemania",
+    precio: 52000
+  },
+  {
+    marca: "Cupra",
+    modelo: "Formentor VZ5",
+    pais: "Países Bajos",
+    precio: 47000
+  }
+];
+
+console.log("Atlas IA iniciado");
+console.log(coches);
