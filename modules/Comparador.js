50c81872d2f3382bd4e7e6215f853641c6180562
monterogaraxe <infomonterogaraxe@gmail.com>
2026-07-21 15:30:18 +0200

    Create ComparadorEuropeo object for comparison logic
    Added ComparadorEuropeo object with initial structure for comparison.

--- modules/Comparador.js
+++ modules/Comparador.js
@@ -1,1 +1,38 @@
+/*
+========================================
+COMPARADOR EUROPEO
+Proyecto Atlas 2.0
+========================================
+*/
 
+const ComparadorEuropeo = {
+
+    comparar(resultadoBusqueda){
+
+        return{
+
+            mejorAnuncio:null,
+
+            anuncios:[],
+
+            precioMinimo:0,
+
+            precioMaximo:0,
+
+            precioMedio:0,
+
+            diferenciaMercado:0,
+
+            ahorro:0,
+
+            paisMasBarato:"",
+
+            paisMasCaro:"",
+
+            recomendacion:""
+
+        };
+
+    }
+
+};
