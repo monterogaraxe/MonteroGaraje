7793d6ee931bce4856ec27f07b4c15395b9b8d21
monterogaraxe <infomonterogaraxe@gmail.com>
2026-07-21 23:33:01 +0200

    Create car catalog with models and specifications
    Added a catalog of car models with generations and versions.

--- catalogo.js
+++ catalogo.js
@@ -1,1 +1,123 @@
+/*
+========================================
+CATÁLOGO ATLAS 1.0
+MonteroGaraje
+========================================
+*/
 
+const CATALOGO = {
+
+    Volkswagen: {
+
+        Golf: {
+            generaciones: [
+                "MK1",
+                "MK2",
+                "MK3",
+                "MK4",
+                "MK5",
+                "MK6",
+                "MK7",
+                "MK8"
+            ],
+
+            versiones: [
+                "GTI 8V",
+                "GTI 16V",
+                "G60",
+                "Rallye",
+                "Country",
+                "Limited",
+                "VR6",
+                "R32",
+                "GTD",
+                "Clubsport",
+                "R"
+            ]
+        }
+
+    },
+
+    Audi: {
+
+        RS3: {
+
+            generaciones: [
+                "8P",
+                "8V",
+                "8Y"
+            ],
+
+            versiones: [
+                "Sportback",
+                "Sedan",
+                "Performance",
+                "Carbon Edition"
+            ]
+
+        }
+
+    },
+
+    BMW: {
+
+        M3: {
+
+            generaciones: [
+                "E30",
+                "E36",
+                "E46",
+                "E90",
+                "F80",
+                "G80"
+            ],
+
+            versiones: [
+                "CS",
+                "CSL",
+                "Competition",
+                "Touring"
+            ]
+
+        }
+
+    },
+
+    Ford: {
+
+        "Focus RS": {
+
+            generaciones: [
+                "MK1",
+                "MK2",
+                "MK3"
+            ],
+
+            versiones: [
+                "Standard",
+                "Mountune",
+                "Heritage"
+            ]
+
+        }
+
+    },
+
+    Cupra: {
+
+        Formentor: {
+
+            generaciones: [
+                "Primera generación"
+            ],
+
+            versiones: [
+                "VZ",
+                "VZ5"
+            ]
+
+        }
+
+    }
+
+};
