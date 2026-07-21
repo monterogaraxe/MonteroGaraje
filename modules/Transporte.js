afb5c6d18b6acd530ca66ee950d92f311759b31c
monterogaraxe <infomonterogaraxe@gmail.com>
2026-07-21 15:32:15 +0200

    Create Transporte module with calcular method
    Added initial structure for Transporte module with calcular method.

--- modules/Transporte.js
+++ modules/Transporte.js
@@ -1,1 +1,44 @@
+/*
+========================================
+TRANSPORTE EUROPEO
+Proyecto Atlas 2.0
+========================================
+*/
 
+const Transporte = {
+
+    calcular(datos){
+
+        return{
+
+            paisOrigen:"",
+
+            ciudadOrigen:"",
+
+            paisDestino:"",
+
+            comunidadDestino:"",
+
+            provinciaDestino:"",
+
+            empresas:[],
+
+            mejorEmpresa:null,
+
+            precioMinimo:0,
+
+            precioMaximo:0,
+
+            precioMedio:0,
+
+            diasEstimados:0,
+
+            seguroIncluido:false,
+
+            resumen:""
+
+        };
+
+    }
+
+};
