2e38b93faf58cdcaf556a4088b8b2ce13a95b40d
monterogaraxe <infomonterogaraxe@gmail.com>
2026-07-21 15:23:47 +0200

    Add Buscador object with search functionality

--- modules/Buscador.js
+++ modules/Buscador.js
@@ -1,1 +1,37 @@
+/*
+========================================
+MONTEROGARAJE
+BUSCADOR INTELIGENTE
+Proyecto Atlas 2.0
+========================================
+*/
 
+const Buscador = {
+
+    buscar(texto){
+
+        return{
+
+            consulta:texto,
+
+            resultados:[],
+
+            mejorUnidad:null,
+
+            precioMedio:0,
+
+            precioImportacion:0,
+
+            mejorGestoria:null,
+
+            mejorTransporte:null,
+
+            indiceMontero:0,
+
+            recomendacion:null
+
+        };
+
+    }
+
+};
