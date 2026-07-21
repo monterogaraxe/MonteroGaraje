a75b2e0111d9159651086ceccf68ddc3e62e2966
monterogaraxe <infomonterogaraxe@gmail.com>
2026-07-21 15:34:43 +0200

    Add RadarMontero object with detectar method

--- modules/Radar.js
+++ modules/Radar.js
@@ -1,1 +1,32 @@
+/*
+========================================
+RADAR MONTERO
+Proyecto Atlas 2.0
+========================================
+*/
 
+const RadarMontero = {
+
+    detectar(datos){
+
+        return{
+
+            oportunidades:[],
+
+            cochesColeccion:[],
+
+            seriesLimitadas:[],
+
+            chollos:[],
+
+            inversiones:[],
+
+            alertas:[],
+
+            resumen:""
+
+        };
+
+    }
+
+};
