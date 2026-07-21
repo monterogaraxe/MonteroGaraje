274b819fb4636905d3f6e0a2eeaaf48d18c2abd8
monterogaraxe <infomonterogaraxe@gmail.com>
2026-07-21 15:28:57 +0200

    Create IndiceMontero with calcular method
    Add IndiceMontero object with calcular method returning default values.

--- modules/Índice.js
+++ modules/Índice.js
@@ -1,1 +1,38 @@
+/*
+========================================
+INDICE MONTERO
+Proyecto Atlas 2.0
+========================================
+*/
 
+const IndiceMontero = {
+
+    calcular(datos){
+
+        return{
+
+            puntuacion:0,
+
+            precio:0,
+
+            kilometros:0,
+
+            historial:0,
+
+            estado:0,
+
+            exclusividad:0,
+
+            importacion:0,
+
+            vendedor:0,
+
+            mantenimiento:0,
+
+            resultado:""
+
+        };
+
+    }
+
+};
