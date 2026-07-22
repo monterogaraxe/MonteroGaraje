e8f4b6093123a605acbdcc01c8c0f3342a837c89
monterogaraxe <infomonterogaraxe@gmail.com>
2026-07-22 23:40:23 +0200

    Update Anuncios.js

--- modules/Anuncios.js
+++ modules/Anuncios.js
@@ -2,29 +2,8 @@ const Anuncios = {
 
     iniciar() {
 
-        document.querySelectorAll(".featured .card button").forEach(boton => {
-
-            boton.addEventListener("click", () => {
-
-                const texto = boton.parentElement.querySelector("h3").textContent;
-
-                document.getElementById("busqueda").value = texto;
-
-                buscarCoche();
-
-                window.scrollTo({
-                    top: 0,
-                    behavior: "smooth"
-                });
-
-            });
-
-        });
+        console.log("Anuncios cargados");
 
     }
 
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-    Anuncios.iniciar();
-});
